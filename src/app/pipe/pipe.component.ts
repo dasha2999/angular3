2fb89f3ff4c10b41d0a6773dd96a1b7124d56051
dasha2999 <125147552+dasha2999@users.noreply.github.com>
2023-10-30 20:35:06 +0300

    Add files via upload

---
+++ src/app/pipe/pipe.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pipe',
+  templateUrl: './pipe.component.html',
+  styleUrls: ['./pipe.component.css']
+})
+export class PipeComponent {
+  num = 0.5;
+  PI = 3.141589;
+  str = "String";
+  date = new Date();
+}
